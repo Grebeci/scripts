9fe163f4eb6c9c8e47bf829bad4bfa74535c7dc9
Anna Wilson <31677997+Grebeci@users.noreply.github.com>
2024-03-14 14:50:30 +0800

    Update setup.sh

--- server_setup/centos/setup.sh
+++ server_setup/centos/setup.sh
@@ -1,3 +1,3 @@
 # shell prompt color
-echo "PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> /etc/bashrc
+echo "PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
 source ~/.bashrc
