05c0e66d268b745931901f7ec9ca7ecabce4d390
王鑫鑫 <wangxinxin@dserspro.com>
2024-12-04 18:57:16 +0800

    一键免密操作

--- bigdata/init/one_key_nopasswd.sh
+++ bigdata/init/one_key_nopasswd.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+################################################################################################
+# prerequisite
+# 选择任意一台机器，配置：
+# 1. 集群内所有 ip->host 映射
+# 2. 定义 CLUSTER 变量，包含所有集群节点
+################################################################################################
+
+
 set -eo pipefail
 
 # 检查是否为root用户
@@ -9,7 +17,6 @@ fi
 
 [ -z "$CLUSTER" ] && export CLUSTER="hadoop102 hadoop103 hadoop104"
 export DEBIAN_FRONTEND=noninteractive
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/BigDataScholar/BigDataScholar.github.io/master/bigdata/init/functions.sh)"
 
 apt update && apt upgrade -y
 apt install -y pdsh
@@ -21,12 +28,18 @@ cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 echo "手动运行命令到其他节点"
 echo "echo $(cat ~/.ssh/id_rsa.pub) >> /root/.ssh/authorized_keys"
 read -p "Press any key to continue... " -n1 -s
-sed -i '/^Host/a\ \ \ \ \ \ \ \ StrictHostKeyChecking no' /etc/ssh/ssh_config
-xsync /etc/ssh/ssh_config
-xsync /etc/hosts
-xcall "apt update && apt upgrade -y"
+sed -i /StrictHostKeyChecking/d /etc/ssh/ssh_config
+sed -i -e '/^Host/a\ \ \ \ \ \ \ \ StrictHostKeyChecking no' /etc/ssh/ssh_config
+for host in $CLUSTER; do
+  rsync -avz /etc/hosts "$host":/etc/
+  rsync -avz /etc/ssh/ssh_config "$host":/etc/ssh/
+  rsync -avz /root/.ssh "$host":/root/
+done
 
-useradd -m -s /bin/bash bigdata
+source <(curl -fsSL https://raw.githubusercontent.com/Grebeci/scripts/refs/heads/main/bigdata/init/functions.sh)
+xcall " export DEBIAN_FRONTEND=noninteractive; apt update && apt upgrade -y"
+
+xcall useradd -m -s /bin/bash bigdata
 rsync -avz /root/.ssh /home/bigdata/
 chown -R bigdata:bigdata /home/bigdata/.ssh
 xsync /home/bigdata/.ssh
